da83ab85da98633d9fc6af2adfb94c393cca6531
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-05-15 21:57:40 -0300

    feat: cria EmailService

---
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/service/EmailService.java
@@ -0,0 +1,22 @@
+package br.com.alura.adopet.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void enviarEmail(String from, String to, String subject, String message) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom(from);
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setText(message);
+        emailSender.send(email);
+    }
+}
