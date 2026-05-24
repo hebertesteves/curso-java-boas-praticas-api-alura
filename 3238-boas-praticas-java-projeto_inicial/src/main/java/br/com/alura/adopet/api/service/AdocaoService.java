57c7614af0c15865215835a70ad305398d6dbb84
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-05-23 21:47:31 -0300

    refac: melhorando encapsulamento

--- 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -42,12 +42,7 @@ public class AdocaoService {
 
         validacoesSolicitacao.forEach(v -> v.validar(dto));
 
-        Adocao adocao = new Adocao();
-        adocao.setData(LocalDateTime.now());
-        adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
-        adocao.setPet(pet);
-        adocao.setTutor(tutor);
-        adocao.setMotivo(dto.motivo());
+        Adocao adocao = new Adocao(tutor, pet, dto.motivo());
 
         repository.save(adocao);
 
@@ -60,7 +55,7 @@ public class AdocaoService {
 
     public void aprovar(AprovacaoAdocaoDTO dto) {
         Adocao adocao = repository.getReferenceById(dto.idAdocao());
-        adocao.setStatus(StatusAdocao.APROVADO);
+        adocao.marcarComoAprovada();
 
         repository.save(adocao);
 
@@ -73,8 +68,7 @@ public class AdocaoService {
 
     public void reprovar(ReprovacaoAdocaoDTO dto) {
         Adocao adocao = repository.getReferenceById(dto.idAdocao());
-        adocao.setStatus(StatusAdocao.REPROVADO);
-        adocao.setJustificativaStatus(dto.justificativa());
+        adocao.marcarComoReprovada(dto.justificativa());
 
         repository.save(adocao);
 
