12a5bc1ec78f44187c21884557d3b7aa51588d52
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-05-23 21:35:10 -0300

    refac: otimizando consultas

--- 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
@@ -18,18 +18,13 @@ public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdo
     @Autowired
     private AdocaoRepository adocaoRepository;
 
-    @Autowired
-    private PetRepository petRepository;
-
     @Override
     public void validar(SolicitacaoAdocaoDTO dto) {
-        Pet pet = petRepository.getReferenceById(dto.idPet());
-        List<Adocao> adocoes = adocaoRepository.findAll();
+        boolean petTemAdocaoEmAndamento = adocaoRepository
+                .existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
 
-        for (Adocao a : adocoes) {
-            if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
-            }
+        if (petTemAdocaoEmAndamento) {
+            throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
         }
     }
 }
