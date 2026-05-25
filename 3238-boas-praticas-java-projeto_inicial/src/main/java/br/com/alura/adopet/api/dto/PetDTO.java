af7fc3a15dc3ca5b51d0d67fe67cfc43decc6e0a
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-05-24 21:37:40 -0300

    feat: cria PetDTO

---
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/dto/PetDTO.java
@@ -0,0 +1,30 @@
+package br.com.alura.adopet.api.dto;
+
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.TipoPet;
+
+public record PetDTO(
+        Long id,
+        TipoPet tipo,
+        String nome,
+        String raca,
+        Integer idade,
+        String cor,
+        Float peso,
+        Boolean adotado,
+        Long abrigoId) {
+
+    public PetDTO(Pet pet) {
+        this(
+                pet.getId(),
+                pet.getTipo(),
+                pet.getNome(),
+                pet.getRaca(),
+                pet.getIdade(),
+                pet.getCor(),
+                pet.getPeso(),
+                pet.getAdotado(),
+                pet.getAbrigo().getId()
+        );
+    }
+}
