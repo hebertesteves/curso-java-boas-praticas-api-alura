32d338afab4e45d381dfff1de1a5942b70a8f0ef
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-05-24 21:20:38 -0300

    feat: cria classe PetService

---
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -0,0 +1,28 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PetService {
+
+    @Autowired
+    private PetRepository repository;
+
+    public ResponseEntity<List<Pet>> listarTodosOsPetsDisponiveis() {
+        List<Pet> pets = repository.findAll();
+        List<Pet> disponiveis = new ArrayList<>();
+        for (Pet pet : pets) {
+            if (pet.getAdotado() == false) {
+                disponiveis.add(pet);
+            }
+        }
+        return ResponseEntity.ok(disponiveis);
+    }
+}
